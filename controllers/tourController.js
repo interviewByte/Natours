dc9c45f2e73eeb01e84868a06b19aa0b572ef2aa
Pavan Kumar Singh <pavanrnri1818@gmail.com>
2025-03-28 09:09:01 +0530

    Initatl Commit

---
+++ controllers/tourController.js
@@ -0,0 +1,95 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.checkID = (req, res, next, val) => {
+  if (req.params.id * 1 > tours.length) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
+
+// 2) ROUTE HANDLERS
+exports.getAllTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    requestAt: req.requestTime,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1;
+  // if (id > tours.length) {
+  //   res.status(404).json({
+  //     status: 'fail',
+  //     message: 'Invalid ID',
+  //   });
+  // }
+  const tour = tours.find((ele) => ele.id === id - 1);
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tour,
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  const id = req.params.id * 1;
+  // if (id > tours.length) {
+  //   res.status(404).json({
+  //     status: 'fail',
+  //     message: 'Invalid ID',
+  //   });
+  // }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Update tour here...>',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
