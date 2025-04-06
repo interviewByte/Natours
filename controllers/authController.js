acf9f01e7fe1c2e6c890f26398d89015efa0b172
Pavan Kumar Singh <pavanrnri1818@gmail.com>
2025-04-06 11:17:47 +0530

    In this commit i have coverd signup and login with jwt and bcryptjs also i have uses some model instance funcuon to decrpyt user password

---
+++ controllers/authController.js
@@ -0,0 +1,40 @@
+var jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/apiError');
+
+const signToken = (id) => {
+  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+  const token = signToken(newUser._id);
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser,
+    },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  // 1) Check email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email amd password', 400));
+  }
+  // 2) check if user exist and password is correct
+  const user = await User.findOne({ email }).select('+password');
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+  // 3) If everything ok, then send token to the client
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
