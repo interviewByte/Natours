8814dee0882f894bb821ccba4ce10afc3487c70c
Pavan Kumar Singh <pavanrnri1818@gmail.com>
2025-04-13 00:17:45 +0530

    In this commit I have added the feature securyty headers middleware

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/apiError');
 const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
-// 1 ) MIDDLEWARES
+// 1 ) GLOBAL MIDDLEWARES
+
+// Set security http header
+app.use(helmet());
+
+// Developmemnt logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// Limit API from the same API
+// Limit request from the same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -23,9 +29,11 @@ const limiter = rateLimit({
   message: 'To many request from this IP, please try again in an hour',
 });
 app.use('/api', limiter);
-app.use(express.json());
+// Body-parser reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+// Serving static file
 app.use(express.static(`${__dirname}/public`));
-
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(req.headers);
