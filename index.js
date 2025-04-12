122c45b42d575cb0cb87cf13ead2ebb416da9dac
Pavan Kumar Singh <pavanrnri1818@gmail.com>
2025-04-13 00:03:30 +0530

    In this commit i have added the feature Implementing the concept of rate limit on one api when request are comes from same IP on our request

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/apiError');
@@ -10,6 +11,18 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit API from the same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  // limit: 3, // Limit each IP to 100 requests per window (here, per 15 minutes).
+  // standardHeaders: 'draft-8', // draft-6: RateLimit-* headers; draft-7 & draft-8: combined RateLimit header
+  // legacyHeaders: false, // Disable the X-RateLimit-* headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+  message: 'To many request from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
